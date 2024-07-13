03adc0aa6b14c6f2abd9daa4194fac9ac2b5eb1d
Mohamednabil1213 <belinabil9090@gmail.com>
2024-07-14 01:53:57 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SingleCustomer from './components/singleCustomer/SingleCustomer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+  <div className='app'>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />}></Route>
+        <Route path='/:id' element={<SingleCustomer />}></Route>
+      </Routes>
+    </BrowserRouter>
     </div>
-  );
+
+  </>
+
+
 }
 
 export default App;
